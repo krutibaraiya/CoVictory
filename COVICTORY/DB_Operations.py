6e59b24712e36a2590ea53803556dd51574b6ba9
Jay Patel <pjay20301@gmail.com>
2021-04-08 10:58:38 +0530

    Made a prototype for the website

--- COVICTORY/DB_Operations.py
+++ COVICTORY/DB_Operations.py
@@ -9,19 +9,56 @@ connection = pymysql.connect(host="localhost",
 cursor = connection.cursor()
 
 
-#fetching and showing data from db
-def get_data():
-    cursor.execute("SELECT * FROM PATIENT")
-    rows = cursor.fetchall()    
-    return rows
+
+def get_centers():
+    cursor.execute("SELECT * FROM VACCINATION_CENTER")
+    centers = cursor.fetchall()    
+    return centers
 
 
-#inserting data to db
+def get_slots(vid):
+    cursor.execute("SELECT vid,s_date,s_time FROM SLOT WHERE vid = %s",(vid))
+    slots = cursor.fetchall()
+    return slots
+
+def get_vid(emailAddress_value):
+    cursor.execute("SELECT vid FROM DOCTOR WHERE d_email = %s",(emailAddress_value))
+    vid = cursor.fetchone()
+    return vid
+
+
+def get_did(emailAddress_value):
+    cursor.execute("SELECT did FROM DOCTOR WHERE d_email = %s",(emailAddress_value))
+    did = cursor.fetchone()
+    return did
+
+
+def get_pid(emailAddress_value):
+    cursor.execute("SELECT pid FROM PATIENT WHERE p_email = %s", (emailAddress_value))
+    pid = cursor.fetchone()
+    return pid
+
+def get_slot_did(vid, slot_date, slot_time):
+    cursor.execute("SELECT did FROM SLOT WHERE vid = %s AND s_date = %s AND s_time = %s", (vid, slot_date, slot_time))
+    did = cursor.fetchone()
+    return did
+
+
+def ifSlotAssigned(vid, slot_date, slot_time):
+    cursor.execute("SELECT * FROM SLOT where vid = %s AND s_date = %s AND s_time = %s",(vid, slot_date, slot_time))
+    slot = cursor.fetchone()
+    return slot
+
+
 def patient_register(firstName_value, lastName_value, emailAddress_value, password1_value, gender_value, dob_value, aadhar_value, phone_value):
     cursor.execute("INSERT INTO PATIENT(pid,fname,lname,p_email,p_pwd,gender,dob,aadhar_id,p_phone) VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)",(firstName_value, lastName_value, emailAddress_value, password1_value, gender_value, dob_value, aadhar_value, phone_value))
     connection.commit()
     return 1
 
+def ifPatientRegistered(emailAddress_value):
+    cursor.execute("SELECT * FROM PATIENT WHERE p_email = %s", (emailAddress_value))
+    account = cursor.fetchone()
+    return account
 
 def ifPatientExist(emailAddress_value, password_value):
     cursor.execute("SELECT * FROM PATIENT WHERE p_email = %s AND p_pwd = %s", (emailAddress_value, password_value))
@@ -29,13 +66,34 @@ def ifPatientExist(emailAddress_value, password_value):
     return account
 
 
-def doctor_register(DID_value, Dname_value, emailAddress_value, password1_value, phone_value):
-    cursor.execute("INSERT INTO DOCTOR(did, dname, d_email, d_pwd, d_phone) VALUES (%s,%s,%s,%s,%s)", (DID_value, Dname_value, emailAddress_value, password1_value, phone_value)) 
+def doctor_register(DID_value, Dname_value, emailAddress_value, password1_value, phone_value, VID_value):
+    cursor.execute("INSERT INTO DOCTOR(did, dname, d_email, d_pwd, d_phone, vid) VALUES (%s,%s,%s,%s,%s,%s)", (DID_value, Dname_value, emailAddress_value, password1_value, phone_value, VID_value)) 
     connection.commit()
     return 1
 
+def ifDoctorRegistered(emailAddress_value):
+    cursor.execute("SELECT * FROM DOCTOR WHERE d_email = %s", (emailAddress_value))
+    account = cursor.fetchone()
+    return account
 
 def ifDoctorExists(emailAddress_value, password1_value):
     cursor.execute("SELECT * FROM DOCTOR WHERE d_email = %s AND d_pwd = %s", (emailAddress_value, password1_value))
     account = cursor.fetchone()
     return account
+
+
+def AssignSlot(vid, slot_date, slot_time, did):
+    cursor.execute("INSERT INTO SLOT(vid, s_date, s_time, did) VALUES (%s, %s, %s, %s)", (vid, slot_date, slot_time, did))
+    connection.commit()
+    return 1
+
+
+def patient_vaccination(pid, vid, did, slot_date, slot_time):
+    cursor.execute("INSERT INTO VACCINATION(pid,vid,did,s_time,s_date) VALUES (%s, %s, %s, %s, %s)",(pid,vid,did,slot_time,slot_date))
+    connection.commit()
+    return 1
+
+def doctor_qualification(did, qualification):
+    cursor.execute("INSERT INTO QUALIFICATION(did, qualification) VALUES (%s, %s)",(did, qualification))
+    connection.commit()
+    return 1
--- COVICTORY/app.py
+++ COVICTORY/app.py
@@ -1,15 +1,35 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+from email_validator import validate_email
 from DB_Operations import *
+import re
+from flask_login import login_required
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-# @app.route("/")
-# def helloworld():
-#     return render_template('patient_reg.html', text="Working")
 
-@app.route("/home")
-def helloworld():
-    all_text = get_data()
-    return render_template('patient-register.html', all_text = all_text)
+
+
+@app.route("/vaccination-center/", methods=["GET", "POST"])
+def VaccinationCenter():
+    centers = get_centers()
+    if request.method == 'POST':
+        session['vid_value'] = request.form["vid"]
+        return redirect(url_for('Slot'))
+    return render_template('vaccination-center.html', centers = centers)
+
+
+@app.route("/slot/", methods=["GET","POST"])
+def Slot():
+    slots = get_slots(session['vid_value'])
+    if request.method == "POST":
+        slot_value = request.form["slot"]
+        slots = slot_value.split(',')
+        session['slot_date'] = slots[0]
+        session['slot_time'] = slots[1]
+        return redirect(url_for('PatientRegister'))
+    return render_template('slot.html', slots = slots)
+
 
 @app.route("/patient-register/", methods=["POST", "GET"])
 def PatientRegister():
@@ -22,11 +42,33 @@ def PatientRegister():
         aadhar_value = request.form["aadhar"]
         phone_value = request.form["phone"]
         password1_value = request.form["password1"]
-        check = ifPatientExist(emailAddress_value, password1_value)
+        password2_value = request.form["password2"]
+
+        valid = validate_email(emailAddress_value)
+        if not valid:
+            flash(u'Enter a valid email address', 'error')
+        
+        aadhar_valid = re.search("[0-9]{12}", aadhar_value)
+        if not aadhar_valid:
+            flash(u'Enter a 12 digit aadhar number', 'error')
+        
+        phone_valid = re.search("[0-9]{10}", phone_value)
+        if not phone_valid:
+            flash(u'Enter a 10 digit phone number', 'error')
+        
+        if password1_value != password2_value:
+            flash(u'Password and Re-enter password field should match','error')
+
+
+        check = ifPatientRegistered(emailAddress_value)
         if not check:
-            register = patient_register(firstName_value, lastName_value, emailAddress_value, password1_value, gender_value, dob_value, aadhar_value, phone_value)    
-    else:
-        return render_template('patient-register.html')
+            patient_register(firstName_value, lastName_value, emailAddress_value, password1_value, gender_value, dob_value, aadhar_value, phone_value)
+            pid = get_pid(emailAddress_value)
+            did = get_slot_did(session['vid_value'], session['slot_date'], session['slot_time'])
+            patient_vaccination(pid, session['vid_value'], did, session['slot_date'], session['slot_time'])
+            return redirect(url_for('PatientLogin'))    
+        else:
+            return render_template('patient-register.html')
     return render_template('patient-register.html')
 
 
@@ -50,15 +92,21 @@ def PatientLogin():
 def DoctorRegister():
     if request.method == "POST":
         DID_value = request.form["DID"]
+        VID_value = request.form["VID"]
         Dname_value = request.form["Dname"]
         emailAddress_value = request.form["emailAddress"]
+        qualifications_value = request.form["qualifications"]
         phone_value = request.form["phone"]
         password1_value = request.form["password1"]
 
-        check = ifDoctorExists(emailAddress_value, password1_value)
+        check = ifDoctorRegistered(emailAddress_value)
         if not check:
-            register = doctor_register(DID_value, Dname_value, emailAddress_value, password1_value, phone_value)
-            return redirect(url_for('doctor-login'))
+            doctor_register(DID_value, Dname_value, emailAddress_value, password1_value, phone_value, VID_value)
+            did = get_did(emailAddress_value)
+            qualifications = qualifications_value.split(',')
+            for qualification in qualifications:
+                doctor_qualification(did,qualification)
+            return redirect(url_for('DoctorLogin'))
     return render_template('doctor-register.html')    
 
 
@@ -67,7 +115,9 @@ def DoctorLogin():
     if request.method == "POST":
         emailAddress_value = request.form["emailAddress"]
         password_value = request.form["password"]
-
+        did = get_did(emailAddress_value)
+        session['emailAddress'] = emailAddress_value
+        session['did'] = did
         check = ifDoctorExists(emailAddress_value, password_value)
 
         if not check:
@@ -75,7 +125,25 @@ def DoctorLogin():
             return render_template('patient-login.html')
         else:
             return "Paient List"
-    return render_template('patient-login.html')
+    return render_template('doctor-login.html')
+
+@login_required
+@app.route("/doctor-slot/", methods=["GET", "POST"])
+def DoctorSlot():
+    if request.method == 'POST':
+       slot_date = request.form["slot_date"]
+       slot_time = request.form["slot_time"]
+       vid = get_vid(session['emailAddress'])
+       check = ifSlotAssigned(vid, slot_date, slot_time)
+
+       if not check:
+           AssignSlot(vid, slot_date, slot_time, session['did'])
+           return render_template('doctor-slot.html')
+       else:
+            flash('This slot is already assigned to other doctor. Please select a different slot.', 'error')
+            return render_template('doctor-slot.html') 
+    return render_template('doctor-slot.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
